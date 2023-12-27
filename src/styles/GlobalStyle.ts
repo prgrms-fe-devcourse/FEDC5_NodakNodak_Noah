7e5c5cabee61623648209088c8c156e2b83afc88
hj9118 <hwl9118@gmail.com>
2023-12-27 14:34:09 +0900

    style: 전역 스타일링 진행

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css");
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap');
+
+  body{
+    font-family: 'Pretendard Variable', 'Noto Sans KR', sans-serif;
+  }
+`;
+export default GlobalStyle;
