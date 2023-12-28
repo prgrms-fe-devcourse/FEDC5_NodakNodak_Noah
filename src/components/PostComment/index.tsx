3a9db6f65364279d94c27fcffb69670e6000a946
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-12-28 15:44:31 +0900

    feat : Post-Comment 컴포넌트를 구현했습니다.
    몇몇곳에서 충돌이 발생할 것으로 예상해서 수정하지 않은 부분들이 있습니다. 추후에 각 컴포넌트 Props의 변경에따라 refector이 필요합니다.

---
+++ src/components/PostComment/index.tsx
@@ -0,0 +1,54 @@
+import Comment from '@/components/Comment';
+import theme from '@/styles/theme';
+import Input from '@/components/Input';
+import Button from '@/components/Button';
+
+const PostComment = () => {
+  return (
+    <div style={{ margin: '3rem 15.19rem' }}>
+      <hr
+        style={{
+          width: '952px',
+          height: '1.48px',
+          color: `${theme.colors.grayscale[200]}`,
+        }}
+      />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        <Comment></Comment>
+        <Comment></Comment>
+        <Comment></Comment>
+        <div
+          className='userInput'
+          style={{
+            display: 'flex',
+            marginTop: '16px',
+            width: '712px',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <Input
+            flex={false}
+            borderType='filled'
+            placeholder='플레이스 홀더 텍스트'
+            width='588px'
+            height='48px'
+          />
+          <Button
+            event='enabled'
+            size='regular'
+            styleType='ghost'
+            type='button'>
+            제출
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostComment;
