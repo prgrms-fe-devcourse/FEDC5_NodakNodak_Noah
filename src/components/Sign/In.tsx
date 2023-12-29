995165df475b557aa766a82937da66f127fa5aa7
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2023-12-29 10:40:15 +0900

    feat: 로그인 회원가입

--- src/components/Sign/In.tsx
+++ src/components/Sign/In.tsx
@@ -1,8 +1,31 @@
 import { Form, Guide, Register, SignProps } from './SignStyle';
 import Input from '../Input';
 import Button from '../Button';
+import PasswordInput from '../Input/PasswordInput';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const In = ({ isLogin, setIsLogin }: SignProps) => {
+  const [email, setEmail] = useState('');
+  const [pw, setPW] = useState('');
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api', {
+        path: 'login',
+        method: 'POST',
+        data: {
+          email: email,
+          password: pw,
+        },
+      });
+      localStorage.setItem('auth-token', data.token);
+      alert('로그인 성공');
+    } catch (e) {
+      alert(e);
+    }
+  };
+
   return (
     <Form>
       <Input
@@ -12,14 +35,15 @@ const In = ({ isLogin, setIsLogin }: SignProps) => {
         placeholder='Email'
         autoComplete='on'
         type='email'
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <Input
+      <PasswordInput
         width='360px'
         height='48px'
         fontSize='20px'
         placeholder='Password'
         autoComplete='off'
-        type='password'
+        onChange={(e) => setPW(e.target.value)}
       />
       <Guide>
         노닥노닥이 처음이라면?
@@ -28,7 +52,11 @@ const In = ({ isLogin, setIsLogin }: SignProps) => {
           <span className='material-symbols-outlined'>arrow_forward_ios</span>
         </Register>
       </Guide>
-      <Button size='wide' styleType='primary' event='enabled'>
+      <Button
+        size='wide'
+        styleType='primary'
+        event='enabled'
+        onClick={handleLogin}>
         로그인
       </Button>
     </Form>
