7ac000585c7a94a0de28c6960ac1abe419fcbc7c
ienrum <azang0506@gmail.com>
2023-12-29 12:49:10 +0900

    style: 메인 페이지 css 추가 및 PostCard 스타일 수정

---
+++ src/pages/Main/StyledMain.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const MainWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+
+  padding: 100px;
+`;
+
+export const PostContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  gap: 20px;
+`;
