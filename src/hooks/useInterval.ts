0a1c0478aa3141b3a920446b753da12923352a2e
안현진 <hyeonjinan096@naver.com>
2024-01-01 13:32:55 +0900

    feat: custom 훅 작성

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,17 @@
+﻿import useIntervalFn from './useIntervalFn';
+import { useEffect } from 'react';
+
+type CallbackFunction = () => void;
+
+const useInterval = (fn: CallbackFunction, ms: number) => {
+  const [run, clear] = useIntervalFn(fn, ms);
+
+  useEffect(() => {
+    run();
+    return clear;
+  }, [run, clear]);
+
+  return clear;
+};
+
+export default useInterval;
---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,30 @@
+﻿import { useState } from 'react';
+
+const useLocalStorage = (key: string, initialValue: string | object) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key);
+
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: string | object) => {
+    try {
+      const valueToStore =
+        typeof value === 'function' ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+      localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
+
+export default useLocalStorage;
