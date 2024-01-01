7b9cd0f916bbdb87a9473c7757de495ddcecf478
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-01-01 21:01:49 +0900

    fix: header에 로고 누르면 메인페이지로 가게 수정

--- src/components/Header/StyledHeader.ts
+++ src/components/Header/StyledHeader.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 
-export const StyledHeaderWrapper = styled.div`
+export const StyledHeaderWrapper = styled.nav`
   width: 80%;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
+export const LogoWrapper = styled.div`
+  cursor: pointer;
+  user-select: none;
+`;
 export const ChannelWrapper = styled.div`
   display: flex;
   justify-content: center;
@@ -17,8 +21,8 @@ export const ChannelWrapper = styled.div`
 
 export const AuthUiWrapper = styled.div`
   display: flex;
+  position: relative;
   justify-content: center;
   align-items: center;
-
   gap: 16px;
 `;
