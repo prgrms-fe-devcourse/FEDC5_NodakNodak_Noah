cf01db161779fb4a864b1f8f710521e1a3785c93
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-01-01 13:00:55 +0900

    fix: api error

--- api/index.ts
+++ api/index.ts
@@ -3,14 +3,20 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   const { path = '', method = 'GET', token = '', data } = req.body;
-  const { data: responseValue } = await axios({
-    url: `${process.env.BASE_URL}/${path}`,
-    method,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    data,
-  });
-  res.status(200).json(responseValue);
+  try {
+    const { data: responseValue } = await axios({
+      url: `${process.env.BASE_URL}/${path}`,
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      data,
+    });
+    res.status(200).json(responseValue);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(400).json(err);
+    }
+  }
 }
