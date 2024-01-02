27d2c08d80faba5bfcfaf68d665b634c01624788
ienrum <azang0506@gmail.com>
2024-01-02 09:26:06 +0900

    feat: #75 커스텀 훅 구현

---
+++ src/hooks/useClickAway.ts
@@ -0,0 +1,35 @@
+﻿import { useEffect, useRef } from 'react';
+
+const events = ['mousedown', 'touchstart'] as const;
+
+const useClickAway = (handler: (e: MouseEvent | TouchEvent) => void) => {
+  const ref = useRef<HTMLElement>(null);
+  const savedHandler = useRef(handler);
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    const handleEvent = (e: MouseEvent | TouchEvent) => {
+      !element.contains(e.target as Node) && savedHandler.current(e);
+    };
+
+    for (const eventName of events) {
+      document.addEventListener(eventName, handleEvent);
+    }
+
+    return () => {
+      for (const eventName of events) {
+        document.removeEventListener(eventName, handleEvent);
+      }
+    };
+  }, [ref]);
+
+  return ref;
+};
+
+export default useClickAway;
