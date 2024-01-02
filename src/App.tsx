5b79154fa7683cd5fad84e0a679d816457647eb5
ienrum <azang0506@gmail.com>
2024-01-02 17:50:33 +0900

    feat: App 에 채널 데이터 가져오기 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
-import { mockChannels } from './components/Header/mockChannels';
 import { mockUsers } from './components/UserListCard/mockUsers';
+import { getChannel } from './slices/channel';
+import { useDispatch, RootState } from './store';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 const App = () => {
+  const channels = useSelector((state: RootState) => state.channel.channels);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getChannel());
+  }, [dispatch]);
   return (
     <>
       <Header
-        channels={mockChannels}
+        channels={channels}
         isAuth={!!localStorage.getItem('auth-token')}
         userImage={mockUsers[0].image}
       />
