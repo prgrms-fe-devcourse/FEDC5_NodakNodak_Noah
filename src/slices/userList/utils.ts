173f9b3c224ce4f6b1d914d23f13c4d0d66d4993
ienrum <azang0506@gmail.com>
2024-01-03 17:22:38 +0900

    refactor: 더 간결한 코드로 리팩터링

--- src/slices/userList/utils.ts
+++ src/slices/userList/utils.ts
@@ -1,16 +1,11 @@
 import { User } from '@/types/APIResponseTypes';
 import { UserSnippet } from '@/components/UserListCard/UserListCardTypes';
 
-export const userListToUserSnippetList = (userList: User[]): UserSnippet[] => {
-  const userSnippetList = userList.map(
-    ({ _id, fullName, image, isOnline }) => ({
-      _id,
-      fullName,
-      isOnline,
-      isFollowing: false,
-      image,
-    }),
-  );
-
-  return userSnippetList;
-};
+export const userListToUserSnippetList = (userList: User[]): UserSnippet[] =>
+  userList.map(({ _id, fullName, image, isOnline }) => ({
+    _id,
+    fullName,
+    isOnline,
+    isFollowing: false,
+    image,
+  }));
