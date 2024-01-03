47d5c363c6761351f549b0e2e98dc10ef76ebca2
hj9118 <hwl9118@gmail.com>
2024-01-03 09:29:23 +0900

    feat: 프로필 수정 버튼 추가

--- src/pages/userPage/index.tsx
+++ src/pages/userPage/index.tsx
@@ -1,13 +1,23 @@
 import UserInfo from './UserInfo';
 import UserPostList from './UserPostList';
+import styled from 'styled-components';
+import Button from '@/components/Button';
 
 const UserPage = () => {
   return (
     <>
+      <RightButton>
+        <Button styleType='ghost'>프로필 수정</Button>
+      </RightButton>
       <UserInfo />
       <UserPostList />
     </>
   );
 };
+
+const RightButton = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
 
 export default UserPage;
