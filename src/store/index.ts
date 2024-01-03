878cc5392ec5332ba04ec67f5db125d21cd00b69
ienrum <azang0506@gmail.com>
2024-01-03 15:14:14 +0900

    feat: store 에 리듀서들 등록

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,16 @@ import { useDispatch as reduxUseDispatch } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import channelReducer from '@/slices/channel';
 import postDetailReducer from '@/slices/postDetail';
+import postListReducer from '@/slices/postList';
+import userListReducer from '@/slices/userList';
 
 const store = configureStore({
-  reducer: { channel: channelReducer, postDetail: postDetailReducer },
+  reducer: {
+    channel: channelReducer,
+    postDetail: postDetailReducer,
+    postList: postListReducer,
+    userList: userListReducer,
+  },
 });
 type AppDispatch = typeof store.dispatch;
 export const useDispatch = () => reduxUseDispatch<AppDispatch>();
