48fd94df1be856ee320dad6c3ff69a6bc3ca9a2e
ienrum <azang0506@gmail.com>
2024-01-03 15:16:23 +0900

    feat: #88 포스트 리스트와 유저 리스트 메인에 출력
    - 6초에 한번씩 유저정보 갱신 (주석 처리함)

--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,35 +1,33 @@
 import { MainWrapper, PostContentWrapper, MainFlexWrapper } from './StyledMain';
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import PostCard from '@/components/PostCard';
 import Pagination from '@/components/Pagination';
 import UserListCard from '@/components/UserListCard';
-import { mockPosts } from '@/components/PostCard/mockPosts';
-import { mockUsers } from '@/components/UserListCard/mockUsers';
 import Text from '@/components/Text';
 import Button from '@/components/Button';
-import { TempPost } from '@/components/PostCard/PostCardTypes';
-import { RootState } from '@/store';
+import { useDispatch } from '@/store';
+import { getPostListByChannelId } from '@/slices/postList/thunks';
+import { getUserList } from '@/slices/userList/thunk';
+import { useSelectedPostList } from '@/hooks/useSelectedPostList';
+import {
+  useSelectedChannel,
+  useSelectedChannelLoading,
+} from '@/hooks/useSelectedChannel';
+import { useSelectedUserList } from '@/hooks/useSelectedUserList';
+// import useInterval from '@/hooks/useInterval'; // polling 방식 , 너무 많은 요청이 갈까봐 주석처리
+import { userListToUserSnippetList } from '@/slices/userList/utils';
+import { usePagination } from '@/hooks/usePagination';
 
 const Main = () => {
-  const [page, setPage] = useState(1);
   const navigate = useNavigate();
-  const channel = useSelector(
-    (state: RootState) => state.channel.currentChannel,
-  );
-  const channelLoading = useSelector(
-    (state: RootState) => state.channel.isLoading,
-  );
+  const dispatch = useDispatch();
 
-  const limit = 12;
-  const posts: TempPost[] = mockPosts.slice((page - 1) * limit, page * limit);
-  const totalPage = Math.ceil(mockPosts.length / limit);
-
-  const handlePageChange = (page: number) => {
-    if (page < 1 || page > totalPage) return;
-    setPage(page);
-  };
+  const userList = useSelectedUserList();
+  const channel = useSelectedChannel();
+  const channelLoading = useSelectedChannelLoading();
+  const { paginationedPostList, totalPage, currentPage, handlePageChange } =
+    usePagination(useSelectedPostList());
 
   const handleWriteClick = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -41,30 +39,46 @@ const Main = () => {
     navigate('/write');
   };
 
+  useEffect(() => {
+    if (!channel || !('_id' in channel)) return;
+    dispatch(getPostListByChannelId({ channelId: channel._id }));
+  }, [dispatch, channel]);
+
+  useEffect(() => {
+    dispatch(getUserList());
+  }, [dispatch]);
+
+  // polling 방식 , 너무 많은 요청이 갈까봐 주석처리
+  // useInterval(() => {
+  //   dispatch(getUserList());
+  // }, 6000);
+
   return (
     <>
       <MainWrapper>
         <PostContentWrapper>
           <MainFlexWrapper>
             <Text tagType='span' fontType='h2'>
-              {!channelLoading && channel ? `${channel.name} 채널` : '채널'}
+              {!channelLoading && channel
+                ? `${channel.name} 채널`
+                : 'loading...'}
             </Text>
             <Button styleType='ghost' size='small' onClick={handleWriteClick}>
               글 쓰기
             </Button>
           </MainFlexWrapper>
           <PostCard.Group>
-            {posts.map((post) => (
+            {paginationedPostList.map((post) => (
               <PostCard key={post._id} post={post} />
             ))}
           </PostCard.Group>
           <Pagination
-            page={page}
+            page={currentPage}
             totalPage={totalPage}
             handlePageChange={handlePageChange}
           />
         </PostContentWrapper>
-        <UserListCard users={mockUsers} />
+        <UserListCard users={userListToUserSnippetList(userList)} />
       </MainWrapper>
     </>
   );
