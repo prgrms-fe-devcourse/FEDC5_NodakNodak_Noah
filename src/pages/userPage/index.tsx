5de803497b07b82a02051d6133db8009c9f39cfa
hj9118 <hwl9118@gmail.com>
2024-01-03 10:06:27 +0900

    fix: setting 모달에서 페이지 전환으로 변경

--- src/pages/userPage/index.tsx
+++ src/pages/userPage/index.tsx
@@ -2,40 +2,23 @@ import UserInfo from './UserInfo';
 import UserPostList from './UserPostList';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 import Button from '@/components/Button';
-import Setting from '@/pages/SettingModal/Setting';
 
 const UserPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
-
-  const openModal = () => {
-    setIsModalOpen(true);
+  const setting = () => {
     navigate('/user/setting');
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    navigate(-1);
   };
 
   return (
     <>
       <RightButton>
-        <Button styleType='ghost' onClick={openModal}>
+        <Button styleType='ghost' onClick={setting}>
           프로필 수정
         </Button>
       </RightButton>
       <UserInfo />
       <UserPostList />
-
-      {isModalOpen && (
-        <div>
-          <div onClick={closeModal}>Close</div>
-          <Setting />
-        </div>
-      )}
     </>
   );
 };
