9fe37081c7225a32349e6ee6734014651e44d3d8
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-01-03 12:53:27 +0900

    feat: Header에 Search 추가

--- src/components/Header/StyledHeader.ts
+++ src/components/Header/StyledHeader.ts
@@ -11,14 +11,24 @@ export const LogoWrapper = styled.div`
   cursor: pointer;
   user-select: none;
 `;
+
 export const ChannelWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
   gap: 16px;
 `;
 
+export const FormContainer = styled.form`
+  position: relative;
+  width: 160px;
+`;
+
+export const SearchIcon = styled.span`
+  position: absolute;
+  left: 32px;
+  top: 4px;
+`;
 export const AuthUiWrapper = styled.div`
   display: flex;
   position: relative;
