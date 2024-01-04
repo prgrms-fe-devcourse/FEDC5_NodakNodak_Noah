a2a104a73ebda756ad5992694273ceacd8e8a51f
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-01-04 13:19:36 +0900

    fix: defaultSrc없이 default 이미지 적용되게 수정 그 외 필요가 없어진 코드 제거

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,40 +1,19 @@
-import { ImageProps } from './ImageProps';
-import { useRef, useState, useEffect } from 'react';
-
 const Image = ({
-  defaultSrc = '/public/DefaultImage.jpg',
   src,
   width,
   height,
   alt,
   ...props
-}: ImageProps) => {
-  const [error, setError] = useState(false);
-  const imgRef = useRef<HTMLImageElement | null>(null);
-
+}: React.ImgHTMLAttributes<HTMLImageElement>) => {
   const imageStyle = {
     display: 'block',
     width,
     height,
     ...props.style,
   };
-  useEffect(() => {
-    if (!src) {
-      setError(true);
-    }
-  }, []);
-  const handleImageError = () => {
-    setError(true);
-  };
 
   return (
-    <img
-      ref={imgRef}
-      src={!error ? src : defaultSrc}
-      alt={alt}
-      style={{ ...imageStyle, objectFit: 'cover' }}
-      onError={handleImageError}
-    />
+    <img src={src} alt={alt} style={{ ...imageStyle, objectFit: 'cover' }} />
   );
 };
 
