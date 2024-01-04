cfb6bc535388504e62e3e90fc89bcd4498cae151
ienrum <azang0506@gmail.com>
2024-01-04 13:10:39 +0900

    feat: detail 투표 페이지 스크롤바 추가

--- src/pages/DetailPage/PostVote/index.tsx
+++ src/pages/DetailPage/PostVote/index.tsx
@@ -4,6 +4,7 @@ import Card from '@/components/Card';
 import Input from '@/components/Input';
 import Button from '@/components/Button';
 import Text from '@/components/Text';
+import ScrollBar from '@/components/ScrollBar';
 
 const VoteTitleWrapper = styled.div`
   display: flex;
@@ -30,37 +31,39 @@ const PostVote = () => {
 
   return (
     <Card width='44.375rem' height='31.25rem' shadowType='large'>
-      <VoteTitleWrapper>
-        <Text
-          tagType='span'
-          fontType='h3'
-          colorType='black'
-          style={{ margin: '1rem 0' }}>
-          {voteTitle}
-        </Text>
-        <Text tagType='span' fontType='body2' colorType='black'>
-          0명 투표
-        </Text>
-      </VoteTitleWrapper>
-      <InputWrapper>
-        {voteArray.map((vote: string, index: number) => (
-          <Input
-            key={index}
-            placeholder={vote}
-            bordertype='enabled'
-            readOnly={true}
-            style={{ marginBottom: '1.5rem', width: '466px', height: '48px' }}
-          />
-        ))}
-      </InputWrapper>
-      <ButtonWrapper>
-        <Button event='enabled' styleType='primary' size='wide'>
-          투표 하기
-        </Button>
-        <Button event='enabled' styleType='ghost' size='wide'>
-          결과 보기
-        </Button>
-      </ButtonWrapper>
+      <ScrollBar>
+        <VoteTitleWrapper>
+          <Text
+            tagType='span'
+            fontType='h3'
+            colorType='black'
+            style={{ margin: '1rem 0' }}>
+            {voteTitle}
+          </Text>
+          <Text tagType='span' fontType='body2' colorType='black'>
+            0명 투표
+          </Text>
+        </VoteTitleWrapper>
+        <InputWrapper>
+          {voteArray.map((vote: string, index: number) => (
+            <Input
+              key={index}
+              placeholder={vote}
+              bordertype='enabled'
+              readOnly={true}
+              style={{ marginBottom: '1.5rem', width: '466px', height: '48px' }}
+            />
+          ))}
+        </InputWrapper>
+        <ButtonWrapper>
+          <Button event='enabled' styleType='primary' size='wide'>
+            투표 하기
+          </Button>
+          <Button event='enabled' styleType='ghost' size='wide'>
+            결과 보기
+          </Button>
+        </ButtonWrapper>
+      </ScrollBar>
     </Card>
   );
 };
