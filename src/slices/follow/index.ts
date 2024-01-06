219751c326a30c77c23e9f7131ba2d37f4dd9695
ienrum <azang0506@gmail.com>
2024-01-06 20:34:27 +0900

    refactor: #128 follow 슬라이스의 isLoading 한번에 처리

--- src/slices/follow/index.ts
+++ src/slices/follow/index.ts
@@ -2,7 +2,7 @@ import { follow, unfollow } from './thunk';
 import { name } from './constants';
 import { InitialState } from './followType';
 import { getUser, getMyInfo } from '../user';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { User } from '@/types/APIResponseTypes';
 
 const initialState: InitialState = {
@@ -24,40 +24,16 @@ export const userInfo = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(follow.pending, (state) => {
-      state.isLoading = true;
-    });
     builder.addCase(follow.fulfilled, (state, action) => {
       state.followData.followId = action.payload._id;
       state.followData.isFollowing = true;
-      state.isLoading = false;
-    });
-    builder.addCase(follow.rejected, (state) => {
-      state.isLoading = false;
-    });
-    builder.addCase(unfollow.pending, (state) => {
-      state.isLoading = true;
     });
     builder.addCase(unfollow.fulfilled, (state, action) => {
       state.followData.followId = action.payload._id;
       state.followData.isFollowing = false;
-      state.isLoading = false;
-    });
-    builder.addCase(unfollow.rejected, (state) => {
-      state.isLoading = false;
-    });
-    builder.addCase(getUser.pending, (state) => {
-      state.isLoading = true;
     });
     builder.addCase(getUser.fulfilled, (state, action) => {
       state.targetUser = action.payload;
-      state.isLoading = false;
-    });
-    builder.addCase(getUser.rejected, (state) => {
-      state.isLoading = false;
-    });
-    builder.addCase(getMyInfo.pending, (state) => {
-      state.isLoading = true;
     });
     builder.addCase(getMyInfo.fulfilled, (state, action) => {
       state.followData.isFollower = (action.payload as User)?.followers?.some(
@@ -69,11 +45,27 @@ export const userInfo = createSlice({
       state.followData.followId = (action.payload as User)?.following?.find(
         ({ user }) => user === state.targetUser?._id,
       )?._id;
-      state.isLoading = false;
     });
-    builder.addCase(getMyInfo.rejected, (state) => {
-      state.isLoading = false;
-    });
+
+    builder.addMatcher(
+      isAnyOf(follow.pending, unfollow.pending, getUser.pending),
+      (state) => {
+        state.isLoading = true;
+      },
+    );
+    builder.addMatcher(
+      isAnyOf(
+        follow.fulfilled,
+        unfollow.fulfilled,
+        getUser.fulfilled,
+        follow.rejected,
+        unfollow.rejected,
+        getUser.rejected,
+      ),
+      (state) => {
+        state.isLoading = false;
+      },
+    );
   },
 });
 
