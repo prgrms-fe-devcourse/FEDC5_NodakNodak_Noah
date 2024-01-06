cbf6631b0b814df991e82921000c5af4deca94f6
ienrum <azang0506@gmail.com>
2024-01-06 10:09:01 +0900

    refactor: #126 타입 선언 추가

--- src/pages/userPage/GrassTable.tsx
+++ src/pages/userPage/GrassTable.tsx
@@ -2,13 +2,12 @@ import styled from 'styled-components';
 import theme from '@/styles/theme';
 import { useSelectedUser } from '@/hooks/useSelectedUser';
 
+type LightnesType = 100 | 200 | 300 | 400 | 500;
+
 const GrassTable = () => {
   const currentUser = useSelectedUser();
 
-  const grass = Array.from(
-    { length: 30 },
-    (): 100 | 200 | 300 | 400 | 500 => 100,
-  );
+  const grass = Array.from({ length: 30 }, (): LightnesType => 100);
 
   currentUser?.posts.forEach((post) => {
     const date = new Date(post.createdAt);
@@ -49,11 +48,11 @@ const GrassWrapper = styled.div`
   border-radius: 4px;
 `;
 
-const GrassItem = styled.div<{ lightness: 100 | 200 | 300 | 400 | 500 }>`
+const GrassItem = styled.div<{ lightness: LightnesType }>`
   width: 20px;
   height: 20px;
   background-color: ${({ lightness }) => theme.colors.success[lightness]};
   border-radius: 4px;
 
-  box-shadow: 0 0 0 1px ${theme.colors.grayscale[200]};
+  box-shadow: 0 0 0 0.5px ${theme.colors.grayscale[200]};
 `;
