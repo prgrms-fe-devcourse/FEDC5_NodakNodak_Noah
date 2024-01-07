627ac03f46e49a7b8f99b922db94d9c565872f3f
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-01-07 20:36:55 +0900

    chore: 컴포넌트 이동으로 인한 경로 수정

--- src/pages/PostEditPage/PostCreatePage/index.tsx
+++ src/pages/PostEditPage/PostCreatePage/index.tsx
@@ -5,13 +5,13 @@ import {
   StyledTextArea,
   ButtonWrapper,
 } from '../StyledPostEdit';
-import DropdownMenu from '../DropdownMenu';
 import VotedBox from '../VoteEditBox';
 import { useNavigate } from 'react-router-dom';
 import { useFormik } from 'formik';
 import axios from 'axios';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
+import DropdownMenu from '@/components/DropdownMenu';
 
 interface FormType {
   title: string;
