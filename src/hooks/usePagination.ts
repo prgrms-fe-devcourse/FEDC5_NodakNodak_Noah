341bec7102b1da905620599916597a862cd66c85
ienrum <azang0506@gmail.com>
2024-01-07 13:51:30 +0900

    fix: #136 pagination 버그 수정

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PostSnippet } from '@/components/PostCard/PostCardTypes';
 
 export const usePagination = (postList: PostSnippet[], limit: number = 12) => {
   const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setPage(1);
+  }, [postList.length]);
 
   const paginationedPostList: PostSnippet[] = postList.slice(
     (page - 1) * limit,
     page * limit,
   );
 
-  const totalPage = Math.ceil(paginationedPostList.length / limit);
+  const totalPage = Math.ceil(postList.length / limit);
 
   const handlePageChange = (page: number) => {
     if (page < 1 || page > totalPage) return;
