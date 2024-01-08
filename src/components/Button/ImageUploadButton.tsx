5df0b327157238f9ae831b53118614f70028c673
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-01-09 01:20:05 +0900

    fix: ImageUploader 조금 수정

--- src/components/Button/ImageUploadButton.tsx
+++ src/components/Button/ImageUploadButton.tsx
@@ -10,6 +10,7 @@ const ImageUploader = ({
   event = 'enabled',
   type,
   isArrow,
+  children,
   setImage,
   apiParam,
 }: ImageUnloadButtonProps) => {
@@ -51,15 +52,13 @@ const ImageUploader = ({
           type={type}
           isArrow={isArrow}
           onClick={() => selectedFile.current?.click()}>
-          이미지 선택
+          {children}
         </Button>
       ) : (
-        'uploading...'
+        'Uploading...'
       )}
       <InvisibleInput
         type='file'
-        name='imageUpload'
-        id='imageUploader'
         accept='image/*'
         ref={selectedFile}
         onChange={onUploadImage}
