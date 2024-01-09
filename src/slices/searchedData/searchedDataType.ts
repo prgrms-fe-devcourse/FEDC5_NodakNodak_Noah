6e6712d9f26e6eac9d28410ab9a6f317d739ad23
ienrum <azang0506@gmail.com>
2024-01-09 13:49:47 +0900

    fix: seached postList 데이터 안깨지게 수정 및 메인 페이지 리팩토링

--- src/slices/searchedData/searchedDataType.ts
+++ src/slices/searchedData/searchedDataType.ts
@@ -1,4 +1,4 @@
-import { User } from '@/types/APIResponseTypes';
+import { User, Comment } from '@/types/APIResponseTypes';
 
 export interface SearchedData {
   postData: SearchedPost[];
@@ -8,10 +8,11 @@ export interface SearchedData {
 
 export interface SearchedPost {
   likes: string[];
-  comments: string[];
+  comments: Comment[];
   _id: string;
   title: string;
-  author: string;
+  author: User;
   createdAt: string;
   updatedAt: string;
+  image?: string;
 }
