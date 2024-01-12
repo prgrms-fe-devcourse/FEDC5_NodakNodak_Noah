ac656016976a4c7b4a574dc9ac8328d86bc3fd94
ienrum <azang0506@gmail.com>
2024-01-12 18:13:30 +0900

    feat: #234 알림 메세지 리팩토링, ui 리팩토링, 좋아요 버그 수정

--- src/components/layout/Header/NotificationCardBell/index.tsx
+++ src/components/layout/Header/NotificationCardBell/index.tsx
@@ -1,50 +1,98 @@
-// import useInterval from '@/hooks/useInterval';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import Bell from '@/assets/Bell';
+import theme from '@/styles/theme';
+import { useDispatch } from '@/store';
 import {
   seeNotifications,
   getNotificationArray,
 } from '@/slices/notification/thunk';
-import Bell from '@/assets/Bell';
-import { useDispatch } from '@/store';
+import { getPostListByMyId } from '@/slices/postList/thunks';
+import { userToUserSnippet } from '@/slices/userList/utils';
+import useInterval from '@/hooks/useInterval';
 import useClickAway from '@/hooks/useClickAway';
-import { useSelectedUserList } from '@/hooks/useSelectedUserList';
+import { useSelectedMyInfo } from '@/hooks/useSelectedMyInfo';
 import { useSelectedNotifications } from '@/hooks/useSelectedNotifications';
-import { Badge, Button, ScrollBar, Text } from '@/components/common';
-import theme from '@/styles/theme';
+import { useSelectedPostListByMyId } from '@/hooks/useSelectedPostListById';
 import {
   NotificationContainer,
-  NotificationHeader,
   NotificationList,
+  NotificationHeader,
 } from '@/components/layout/Header/NotificationCardBell/style';
+import UserSnippet from '@/components/Main/UserListCard/UserSnippet';
+import { Badge, ScrollBar, Text, Button } from '@/components/common';
 
 const NotificationCardBell = () => {
   const token = localStorage.getItem('auth-token');
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const [toggleNotification, setToggleNotification] = useState(false);
   const notifications = useSelectedNotifications();
-  const userList = useSelectedUserList();
+  const postListByMyId = useSelectedPostListByMyId();
+  const myInfo = useSelectedMyInfo();
   const count = notifications.length;
 
   const notificationsArray = notifications.map(
-    ({ _id, comment, follow, author }) => {
-      const follower =
-        userList.find((user) => user._id === follow?.follower)?.fullName || '';
-      if (comment && 'comment' in comment) {
-        const isVote = JSON.parse(comment.comment).type === 'vote';
-        const text = isVote
-          ? `${author.fullName}님이 투표에 참여했습니다.`
-          : `${author.fullName}님이 댓글을 달았습니다.`;
+    ({ _id, comment, follow, author, post, message }) => {
+      const snippetAuthor = userToUserSnippet(author, myInfo);
+      if (!snippetAuthor) return { _id, text: '알수 없는 알림' };
 
-        return { _id, text };
+      const follower = follow ? snippetAuthor.fullName : '';
+      const postTitleJsonString = postListByMyId.find(
+        (postByUser) => postByUser._id === post,
+      )?.title;
+
+      const postTitle = postTitleJsonString
+        ? JSON.parse(postTitleJsonString).title
+        : '';
+
+      const channelId = postListByMyId.find(
+        (postByUser) => postByUser._id === post,
+      )?.channel._id;
+
+      if (comment && 'comment' in comment && snippetAuthor) {
+        const isVote = JSON.parse(comment.comment).type === 'vote';
+
+        const text = isVote
+          ? `${snippetAuthor.fullName}님이 ${postTitle}글에 투표하였습니다.`
+          : `${snippetAuthor.fullName}님이 ${postTitle}글에 댓글을 달았습니다.`;
+
+        return {
+          _id,
+          text,
+          snippetAuthor,
+          handleClick: channelId
+            ? () => navigate(`/detail/${channelId}/${post}`)
+            : undefined,
+        };
       } else if (follower) {
         const text = `${follower}님이 팔로우했습니다.`;
 
-        return { _id, text };
-      } else {
-        const text = `좋아요가 눌렸습니다.`;
+        return { _id, text, snippetAuthor };
+      } else if (post && !follow && snippetAuthor) {
+        const text = `${snippetAuthor.fullName}님이 ${postTitle}에 좋아요 를 남겼습니다.`;
 
-        return { _id, text };
+        return {
+          _id,
+          text,
+          snippetAuthor,
+          handleClick: channelId
+            ? () => navigate(`/detail/${channelId}/${post}`)
+            : undefined,
+        };
+      } else if (message && snippetAuthor) {
+        const text = `${snippetAuthor.fullName}님이 메세지를 보냈습니다.`;
+
+        return {
+          _id,
+          text,
+          snippetAuthor,
+          handleClick: undefined,
+        };
+      } else {
+        return { _id, text: '알수 없는 알림', snippetAuthor };
       }
     },
   );
@@ -62,14 +110,14 @@ const NotificationCardBell = () => {
 
   useEffect(() => {
     if (!token) return;
+    dispatch(getPostListByMyId());
     dispatch(getNotificationArray());
   }, [dispatch, token]);
 
-  // polling 방식 , 너무 많은 요청이 갈까봐 주석처리
-  // useInterval(() => {
-  //   if (!token) return;
-  //   dispatch(getNotificationArray());
-  // }, 10000);
+  useInterval(() => {
+    if (!token) return;
+    dispatch(getNotificationArray());
+  }, 10000);
 
   return (
     <Badge count={count}>
@@ -78,33 +126,46 @@ const NotificationCardBell = () => {
         <NotificationContainer ref={ref}>
           <ScrollBar>
             <NotificationHeader>
-              <Text
-                tagType='span'
-                fontType='body2'
-                colorType='primary'
-                colorNumber={theme.isDark ? '100' : '500'}>
-                알림
+              <Text tagType='span' fontType='body3'>
+                {`알림 - ${notificationsArray.length}`}
               </Text>
               <Button
                 type='button'
                 size='small'
                 style={{ padding: '8px 4px' }}
+                styleType='danger'
                 onClick={handleSeeNotifications}>
-                모두 읽음
+                알림 삭제
               </Button>
             </NotificationHeader>
             <NotificationList>
-              {notificationsArray.map(({ _id, text }) => (
-                <li
-                  key={_id}
-                  style={{
-                    color: theme.isDark
-                      ? theme.colors.primary[100]
-                      : theme.colors.primary[500],
-                  }}>
-                  {text}
-                </li>
-              ))}
+              <UserSnippet.Group>
+                {notificationsArray.length === 0 && (
+                  <Text tagType='span' fontType='h3'>
+                    {'알림이 없습니다.'}
+                  </Text>
+                )}
+                {notificationsArray.map(
+                  ({ _id, text, snippetAuthor: author, handleClick }) => {
+                    return (
+                      <UserSnippet
+                        key={_id}
+                        userId={author?._id || ''}
+                        image={author?.image || ''}
+                        isOnline={author?.isOnline || false}
+                        isFollowing={author?.isFollowing || false}
+                        handleClick={handleClick}
+                        fullName={text}
+                        style={{
+                          color: theme.isDark
+                            ? theme.colors.primary[100]
+                            : theme.colors.primary[500],
+                        }}
+                      />
+                    );
+                  },
+                )}
+              </UserSnippet.Group>
             </NotificationList>
           </ScrollBar>
         </NotificationContainer>
