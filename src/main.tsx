fbec38a8e07cde1a12d743f4e8b21b3105591db1
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-01-12 12:39:47 +0900

    fix: styled component에서 css import를 하지 말라는 경고 수용

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import ProtectedRoute from '@/components/common/ProtectedRoute';
 
 import App from '@/App';
@@ -85,9 +87,23 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-      <GlobalStyle />
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+        <Helmet>
+          <link
+            rel='stylesheet'
+            type='text/css'
+            href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css'
+          />
+          <link
+            rel='stylesheet'
+            type='text/css'
+            href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap'
+          />
+        </Helmet>
+        <GlobalStyle />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>,
 );
