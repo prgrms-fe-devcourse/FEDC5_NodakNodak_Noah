15f16828d3fc43c30eae03170d6a4d13a8dcb82c
hj9118 <hwl9118@gmail.com>
2024-01-12 15:51:16 +0900

    refactor: 코드 컨벤션 및 가독성 향상

--- src/components/Index/index.tsx
+++ src/components/Index/index.tsx
@@ -1,77 +1,25 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import './styles.css';
 import { Pagination, Navigation } from 'swiper/modules';
 import theme from '@/styles/theme';
+import { StyledSwiper, StyledSwiperSlide } from '@/components/Index/style';
 
 const Swipe = () => {
   return (
-    <>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className='mySwiper'
-        style={{
-          '--swiper-pagination-color': theme.isDark
-            ? theme.colors.primary[200]
-            : theme.colors.primary[400],
-          '--swiper-pagination-bullet-inactive-color': theme.isDark
-            ? theme.colors.grayscale[200]
-            : theme.colors.grayscale[400],
-          '--swiper-pagination-bullet-inactive-o pacity': '0.4',
-          '--swiper-pagination-bullet-size': '8px',
-          '--swiper-pagination-bullet-horizontal-gap': '4px',
-          '--swiper-navigation-size': '28px',
-          '--swiper-theme-color': theme.isDark
-            ? theme.colors.primary[200]
-            : theme.colors.primary[400],
-        }}>
-        <SwiperSlide>
-          {theme.isDark ? (
-            <img src='/public/SlideD0.jpg' />
-          ) : (
-            <img src='/public/SlideL0.jpg' />
-          )}
-        </SwiperSlide>
-        <SwiperSlide>
-          {theme.isDark ? (
-            <img src='/public/SlideD1.jpg' />
-          ) : (
-            <img src='/public/SlideL1.jpg' />
-          )}
-        </SwiperSlide>
-        <SwiperSlide>
-          {theme.isDark ? (
-            <img src='/public/SlideD2.jpg' />
-          ) : (
-            <img src='/public/SlideL2.jpg' />
-          )}
-        </SwiperSlide>
-        <SwiperSlide>
-          {theme.isDark ? (
-            <img src='/public/SlideD3.jpg' />
-          ) : (
-            <img src='/public/SlideL3.jpg' />
-          )}
-        </SwiperSlide>
-        <SwiperSlide>
-          {theme.isDark ? (
-            <img src='/public/SlideD4.jpg' />
-          ) : (
-            <img src='/public/SlideL4.jpg' />
-          )}
-        </SwiperSlide>
-      </Swiper>
-    </>
+    <StyledSwiper
+      slidesPerView={1}
+      spaceBetween={30}
+      loop={true}
+      pagination={{ clickable: true }}
+      navigation={true}
+      modules={[Pagination, Navigation]}>
+      {[0, 1, 2, 3, 4].map((index) => (
+        <StyledSwiperSlide key={index}>
+          <img
+            src={`/public/${theme.isDark ? 'SlideD' : 'SlideL'}${index}.jpg`}
+            alt={`Slide ${index}`}
+          />
+        </StyledSwiperSlide>
+      ))}
+    </StyledSwiper>
   );
 };
 
