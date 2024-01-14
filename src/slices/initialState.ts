765694f1c9719a1ff7e2733c8b981e5bb8c41c20
ienrum <azang0506@gmail.com>
2024-01-15 00:23:26 +0900

    feat: 각 reponse 타입에 대한 initialState 선언

--- src/slices/initialState.ts
+++ src/slices/initialState.ts
@@ -1,4 +1,20 @@
-export const initialUser = {
+import {
+  Channel,
+  Comment,
+  Conversation,
+  Follow,
+  Like,
+  Message,
+  Notification,
+  Post,
+  User,
+} from '@/types/APIResponseTypes';
+import { SearchedPost } from '@/slices/searchedData/type';
+
+export const initialTitle = `{"title":"","content":"","voteTitle":"","voteArray":[""]}`;
+export const initialCustomComment = `{"type":"","content":""}`;
+
+export const initialUser: User = {
   coverImage: '',
   image: '',
   role: '',
@@ -18,4 +34,96 @@ export const initialUser = {
   email: '',
   createdAt: '',
   updatedAt: '',
+};
+
+export const initialChannel: Channel = {
+  authRequired: false,
+  posts: [],
+  _id: '',
+  name: '',
+  description: '',
+  createdAt: '',
+  updatedAt: '',
+};
+
+export const initialPost: Post = {
+  avatar: '',
+  likes: [],
+  comments: [],
+  _id: '',
+  image: '',
+  imagePublicId: '',
+  title: initialTitle,
+  channel: initialChannel,
+  author: initialUser,
+  createdAt: '',
+  updatedAt: '',
+};
+
+export const initialSearchedPost: SearchedPost = {
+  likes: [],
+  comments: [],
+  _id: '',
+  title: initialTitle,
+  author: initialUser,
+  createdAt: '',
+  updatedAt: '',
+  image: '',
+};
+export const initialLike: Like = {
+  _id: '',
+  user: initialUser,
+  post: initialPost,
+  createdAt: '',
+  updatedAt: '',
+};
+
+export const initialFollow: Follow = {
+  _id: '',
+  user: '',
+  follower: '',
+  createdAt: '',
+  updatedAt: '',
+};
+
+export const initialCoversation: Conversation = {
+  _id: [],
+  message: '',
+  sender: initialUser,
+  receiver: initialUser,
+  seen: false,
+  createdAt: '',
+};
+
+export const initialMessage: Message = {
+  _id: '',
+  message: '',
+  sender: initialUser,
+  receiver: initialUser,
+  seen: false,
+  createdAt: '',
+  updatedAt: '',
+};
+
+export const initialComment: Comment = {
+  _id: '',
+  comment: initialCustomComment,
+  author: initialUser,
+  post: '',
+  createdAt: '',
+  updatedAt: '',
+};
+
+export const initialNotification: Notification & { type: string } = {
+  seen: false,
+  _id: '',
+  author: initialUser,
+  user: initialUser,
+  post: null,
+  follow: initialFollow,
+  comment: initialComment,
+  message: '',
+  createdAt: '',
+  updatedAt: '',
+  type: 'COMMENT',
 };
