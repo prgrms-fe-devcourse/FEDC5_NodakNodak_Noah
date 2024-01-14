8c4fab733acfe775651987c61915df558e20f2aa
ienrum <azang0506@gmail.com>
2024-01-14 11:15:53 +0900

    refactor: compound 패턴을 컴포넌트로 따로 추가

---
+++ src/components/Main/UserList/UserGroup/style.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  z-index: 2;
+  width: 100%;
+  padding: 16px 0;
+`;
