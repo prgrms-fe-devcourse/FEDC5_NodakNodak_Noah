6bc8641056fd96affdf3dccf9d55452153775c44
ienrum <azang0506@gmail.com>
2024-01-15 15:35:19 +0900

    feat: 필요한 selected 훅 정의

--- src/hooks/useSelectedUser.ts
+++ src/hooks/useSelectedUser.ts
@@ -3,3 +3,8 @@ import { RootState } from '@/store';
 
 export const useSelectedUser = () =>
   useSelector((state: RootState) => state.userInfo.currentUser);
+
+export const useSelectedUserLoading = () =>
+  useSelector(
+    (state: RootState) => state.userInfo.currentUserStatus === 'loading',
+  );
