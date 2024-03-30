d8a8ba9b38189d83ec3bc0b779d67ef59677b25e
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-30 13:41:16 +0900

    refactor: 응답 모델 타입 선언

---
+++ src/apis/responseModel.ts
@@ -0,0 +1,100 @@
+export interface User {
+  coverImage: string;
+  image: string;
+  role: string;
+  emailVerified: boolean;
+  banned: boolean;
+  isOnline: boolean;
+  posts: Post[];
+  likes: Like[];
+  comments: string[];
+  notifications: Notification[];
+  messages: Message[];
+  followers: string[];
+  following: Follow[]; // __v 추가해줘야함
+  _id: string;
+  fullName: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Post {
+  likes: Like[];
+  comments: Comment[];
+  _id: string;
+  image?: string;
+  imagePublicId?: string;
+  title: string;
+  channel: Channel;
+  author: User;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Channel {
+  authRequired: boolean; // 사용되지 않음
+  posts: string[];
+  _id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Like {
+  _id: string;
+  user: string; // 사용자 id
+  post: string; // 포스트 id
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Comment {
+  _id: string;
+  comment: string;
+  author: User;
+  post: string; // 포스트 id
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Notification {
+  seen: boolean;
+  _id: string;
+  author: User;
+  user: User | string;
+  post: string | null; // 포스트 id
+  follow?: string; // 사용자 id
+  comment?: Comment;
+  message?: string; // 메시지 id
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Follow {
+  _id: string;
+  user: string; // 사용자 id
+  follower: string; // 사용자 id
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Conversation {
+  _id: string[];
+  message: string;
+  sender: User;
+  receiver: User;
+  seen: boolean;
+  createdAt: string;
+}
+
+export interface Message {
+  _id: string;
+  message: string;
+  sender: User;
+  receiver: User;
+  seen: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
