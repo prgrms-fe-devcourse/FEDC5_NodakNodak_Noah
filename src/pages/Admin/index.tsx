e487f7409ccd895aa49bc593f2c3ad064d70098b
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-04-03 00:38:53 +0900

    fix: react query at admin page + loading spinner

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -11,24 +11,22 @@ import {
 } from './style';
 import { Container } from '../Sign/style';
 import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import axiosInstance from '@/utils/customAxios';
 import { Button, Input, Text } from '@/components';
-import { Message } from '@/types/APIResponseTypes';
 import { useDispatch } from '@/store';
 import { getMyInfo } from '@/slices/user/thunk';
+import { Message } from '@/apis/responseModel';
+import { Spinner } from '@/components/MainPageSpinner/style';
 
 const Admin = () => {
-  const [messages, setMessages] = useState([]);
   const [channelName, setChannelName] = useState('');
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const getMessages = async () => {
-      const { data } = await axiosInstance.get('messages/conversations');
-      setMessages(data);
-    };
-    getMessages();
-  }, []);
+  const { data: messages, isLoading } = useQuery({
+    queryKey: ['request'],
+    queryFn: async () => await axiosInstance.get('messages/conversations'),
+  });
 
   const handleSeen = async (id: string) => {
     await axiosInstance.put('messages/update-seen', {
@@ -52,7 +50,7 @@ const Admin = () => {
     <Container>
       <AdminCard>
         <Text tagType='span' fontType='h1' style={{ marginTop: '32px' }}>
-          사용자의 문의 요청
+          사용자 문의 요청
         </Text>
         <Legend key={'Question Legend'}>
           <FullName>문의자</FullName>
@@ -60,34 +58,38 @@ const Admin = () => {
           <RequestDate>문의일</RequestDate>
           <LegendRead>처리 상태</LegendRead>
         </Legend>
-        <RequestUl>
-          {messages.length
-            ? messages.map((message: Message) => {
-                const originalDate = new Date(message.createdAt);
-                const year = originalDate.getFullYear();
-                const month = originalDate.getMonth() + 1;
-                const day = originalDate.getDate();
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <RequestUl>
+            {messages && messages.data.length
+              ? messages.data.map((message: Message) => {
+                  const originalDate = new Date(message.createdAt);
+                  const year = originalDate.getFullYear();
+                  const month = originalDate.getMonth() + 1;
+                  const day = originalDate.getDate();
 
-                return (
-                  <RequestLi
-                    key={message.createdAt}
-                    onClick={() => handleSeen(message.sender._id)}>
-                    <FullName>{message.sender.fullName}</FullName>
-                    <RequestMessage>{message.message}</RequestMessage>
-                    <RequestDate>{`${year}/${month}/${day}`}</RequestDate>
-                    <Button
-                      size='small'
-                      styleType='ghost'
-                      disabled={message.seen}
-                      event={message.seen ? 'disabled' : 'enabled'}
-                      style={{ height: '16px' }}>
-                      {message.seen ? '완료' : '미완료'}
-                    </Button>
-                  </RequestLi>
-                );
-              })
-            : '문의 사항이 없습니다'}
-        </RequestUl>
+                  return (
+                    <RequestLi
+                      key={message.createdAt}
+                      onClick={() => handleSeen(message.sender._id)}>
+                      <FullName>{message.sender.fullName}</FullName>
+                      <RequestMessage>{message.message}</RequestMessage>
+                      <RequestDate>{`${year}/${month}/${day}`}</RequestDate>
+                      <Button
+                        size='small'
+                        styleType='ghost'
+                        disabled={message.seen}
+                        event={message.seen ? 'disabled' : 'enabled'}
+                        style={{ height: '16px' }}>
+                        {message.seen ? '완료' : '미완료'}
+                      </Button>
+                    </RequestLi>
+                  );
+                })
+              : '문의 사항이 없습니다'}
+          </RequestUl>
+        )}
         <ChannelCreator>
           <Input
             height={'32px'}
