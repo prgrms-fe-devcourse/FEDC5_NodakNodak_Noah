6ab58b939b6fb5c7e2d0508be47762571387c6b8
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-04-03 00:35:11 +0900

    feat: react-query 도입 위한 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { Outlet } from 'react-router-dom';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 
 const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: false,
+      },
+    },
+  });
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </QueryClientProvider>
   );
 };
 
