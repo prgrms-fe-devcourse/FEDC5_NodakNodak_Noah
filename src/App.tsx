d5b318272d624a7afdcc597c28aaca9537975555
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-04-03 22:32:04 +0900

    fix: queryclient 컴포넌트 밖으로 빼서 캐시초기화 방지

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 
-const App = () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        retry: false,
-      },
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
     },
-  });
+  },
+});
 
+const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Header />
