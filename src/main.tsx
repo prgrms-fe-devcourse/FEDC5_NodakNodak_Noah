e57894a5e20812cd8a8aca2cd53973c21f4c5e76
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-04-10 15:33:32 +0900

    [#335] Feat: 코드 스플리팅 적용 (#336)
    * refactor: 라우터 코드 분리
    
    * feat: 페이지 단위 code spliting 적용
    
    * fix: Import order 수정
    
    * fix: StrictMode추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,106 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import App from '@/App';
-import ProtectedRoute from '@/components/ProtectedRoute';
-import AdminPage from '@/pages/Admin';
-import IntroducePage from '@/pages/Introduce';
-import MainPage from '@/pages/Main';
-import NotFoundPage from '@/pages/NotFound';
-import PostCreatePage from '@/pages/PostCreate';
-import DetailPage from '@/pages/PostDetail';
-import PostVoteChart from '@/pages/PostDetail/components/Result';
-import PostVote from '@/pages/PostDetail/components/Vote';
-import PostUpdatePage from '@/pages/PostUpdate';
-import UserPage from '@/pages/Profile';
-import ProfileEditPage from '@/pages/ProfileEdit';
-import Password from '@/pages/ProfileEdit/components/Password';
-import UserInfo from '@/pages/ProfileEdit/components/UserInfo';
-import RequestToAdminPage from '@/pages/RequestToAdmin';
-import LoginPage from '@/pages/Sign';
+import { RouterProvider } from 'react-router-dom';
 import store from '@/store';
 import GlobalStyle from '@/styles/GlobalStyle';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <IntroducePage />,
-    errorElement: <NotFoundPage />,
-    index: true,
-  },
-  {
-    path: '/user/:userId/setting/',
-    element: (
-      <ProtectedRoute>
-        <ProfileEditPage />
-      </ProtectedRoute>
-    ),
-    children: [
-      { path: '', element: <UserInfo /> },
-      { path: 'password', element: <Password /> },
-    ],
-  },
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/update/:postId',
-        element: (
-          <ProtectedRoute>
-            <PostUpdatePage />
-          </ProtectedRoute>
-        ),
-      },
-
-      {
-        path: '/write/:channelId',
-        element: (
-          <ProtectedRoute>
-            <PostCreatePage />
-          </ProtectedRoute>
-        ),
-      },
-      { path: '/home/:channelId?', element: <MainPage /> },
-      { path: '/user/:userId', element: <UserPage /> },
-      {
-        path: '/request',
-        element: (
-          <ProtectedRoute>
-            <RequestToAdminPage />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: '/admin',
-        element: (
-          <ProtectedRoute admin>
-            <AdminPage />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: '/detail/:channelId/:postId/',
-        element: (
-          <ProtectedRoute>
-            <DetailPage />
-          </ProtectedRoute>
-        ),
-        children: [
-          { path: '', element: <PostVote /> },
-          { path: 'result', element: <PostVoteChart /> },
-        ],
-      },
-    ],
-  },
-  { path: '/sign', element: <LoginPage /> },
-]);
+import { Spinner } from './components/MainPageSpinner/style';
+import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <Suspense fallback={<Spinner />}>
+        <RouterProvider router={router} />
+      </Suspense>
       <link
         rel='stylesheet'
         type='text/css'
@@ -113,5 +25,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       />
       <GlobalStyle />
     </Provider>
+    ,
   </React.StrictMode>,
 );
