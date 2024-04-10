78cc7eda444099eae2326747551c0179a37252f1
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-04-11 00:39:53 +0900

    feat: getUser api 코드

--- src/apis/getUser.ts
+++ src/apis/getUser.ts
@@ -1,1 +1,18 @@
 // userId를 통해 한명의 사용자 정보 조회
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { api } from './core';
+import type { User } from './responseModel';
+
+const getUser = (userId: string) =>
+  api.get<User>({
+    url: `/user/${userId}`,
+  });
+
+export const useGetUser = (userId: string) => {
+  const { data } = useSuspenseQuery({
+    queryKey: ['getUser', userId],
+    queryFn: () => getUser(userId),
+  });
+
+  return data;
+};
