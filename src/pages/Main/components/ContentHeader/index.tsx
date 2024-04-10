25d78010c2d9315d079eba7006d257be0ed5f74a
ienrum <azang0506@gmail.com>
2024-04-10 20:23:08 +0900

    refactor: content header fetching 로직 제거

--- src/pages/Main/components/ContentHeader/index.tsx
+++ src/pages/Main/components/ContentHeader/index.tsx
@@ -1,39 +1,21 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { Button, Text } from '@/components';
-import { useSelectedChannel } from '@/hooks/useSelectedChannel';
-import { useSelectedStatus } from '@/hooks/useSelectedStatus';
 import { ContentHeaderWrapper } from '@/pages/Main/components/ContentHeader/style';
 
 const ContentHeader = () => {
   const { channelId } = useParams();
   const navigate = useNavigate();
+  const channelName = useLocation().state as string;
 
   const handleWriteClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault();
     navigate(`/write/${channelId ? channelId : 'unselected'}`);
   };
 
-  const channel = useSelectedChannel();
-  const fullPostStatus = useSelectedStatus('get', '/posts');
-  const postByChannelStatue = useSelectedStatus(
-    'get',
-    '/posts/channel/',
-    channelId,
-  );
-
-  const channelTitle = (() => {
-    if (postByChannelStatue.isLoading) return '로딩중';
-    if (!channelId && !fullPostStatus.isLoading) return '전체 글';
-    if (postByChannelStatue.error && !postByChannelStatue.isLoading)
-      return '채널을 찾을 수 없습니다.';
-    if (channel) return channel.name;
-    return '로딩중';
-  })();
-
   return (
     <ContentHeaderWrapper>
       <Text tagType='span' fontType='h2'>
-        {channelTitle}
+        {channelName ? channelName : '전체 글'}
       </Text>
       <Button styleType='ghost' size='small' onClick={handleWriteClick}>
         글 쓰기
