6f2de0ceaa8bfcc79422cc553a9564e37ec3fbcc
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-04-10 15:22:39 +0900

    fix: StrictMode추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
@@ -8,20 +8,23 @@ import { Spinner } from './components/MainPageSpinner/style';
 import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <Suspense fallback={<Spinner />}>
-      <RouterProvider router={router} />
-    </Suspense>
-    <link
-      rel='stylesheet'
-      type='text/css'
-      href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css'
-    />
-    <link
-      rel='stylesheet'
-      type='text/css'
-      href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap'
-    />
-    <GlobalStyle />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <Suspense fallback={<Spinner />}>
+        <RouterProvider router={router} />
+      </Suspense>
+      <link
+        rel='stylesheet'
+        type='text/css'
+        href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css'
+      />
+      <link
+        rel='stylesheet'
+        type='text/css'
+        href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap'
+      />
+      <GlobalStyle />
+    </Provider>
+    ,
+  </React.StrictMode>,
 );
